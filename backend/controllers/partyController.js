9ae06bac6696f044251b3fcd196fef3004dcd33d
andre.vieira <andre.vieira@grupoicts.com.br>
2023-09-19 07:36:09 -0400

    Projeto finalizado

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -6,7 +6,7 @@ const checkPartyBudget = (budget, services) => {
 
     const priceSum = services.reduce((sum, service) => sum + service.price, 0);
 
-    if(priceSum > budget){
+    if (priceSum > budget) {
         return false;
     }
 
@@ -15,7 +15,7 @@ const checkPartyBudget = (budget, services) => {
 };
 
 const partyController = {
-    create: async(req, res) => {
+    create: async (req, res) => {
         try {
 
             const party = {
@@ -27,14 +27,91 @@ const partyController = {
                 services: req.body.services
             };
 
-            if(party.services && !checkPartyBudget(party.budget, party.services)){
-                res.status(406).json({msg: 'O seu orçamento é insuficiente'});
+            if (party.services && !checkPartyBudget(party.budget, party.services)) {
+                res.status(406).json({ msg: 'O seu orçamento é insuficiente' });
                 return;
             }
 
             const response = await PartyModel.create(party);
-            res.status(201).json({response, msg: 'Festa criada com sucesso'});
-            
+            res.status(201).json({ response, msg: 'Festa criada com sucesso' });
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    getAll: async (req, res) => {
+        try {
+
+            const party = await PartyModel.find();
+            res.status(200).json(party);
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    get: async (req, res) => {
+        try {
+
+            const id = req.params.id;
+            const party = await PartyModel.findById(id);
+
+            if (!party) {
+                res.status(404).json({ msg: 'Festa não encontrada' });
+                return;
+            }
+
+            res.status(200).json(party);
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    delete: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const deletedParty = await PartyModel.findByIdAndDelete(id);
+
+            if (!deletedParty) {
+                res.status(404).json({ msg: 'Festa não encontrada' });
+                return;
+            }
+
+            res.status(200).json({ deletedParty, msg: 'Festa apagada com sucesso' });
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    update: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const party = {
+                title: req.body.title,
+                author: req.body.author,
+                description: req.body.description,
+                budget: req.body.budget,
+                image: req.body.image,
+                services: req.body.services
+            };
+
+            if (party.services && !checkPartyBudget(party.budget, party.services)) {
+                res.status(406).json({ msg: 'O seu orçamento é insuficiente' });
+                return;
+            }
+
+            const updatedParty = await PartyModel.findByIdAndUpdate(id, party);
+
+            if (!updatedParty) {
+                res.status(404).json({ msg: 'Festa não encontrada' });
+            }
+
+            res.status(200).json({ party, msg: 'Festa atualizada com sucesso' });
+
+
         } catch (error) {
             console.log(error);
         }
--- backend/routes/parties.js
+++ backend/routes/parties.js
@@ -5,4 +5,12 @@ const router = Router();
 
 router.post('/', (req, res) => partyController.create(req, res));
 
+router.get('/', (req, res) => partyController.getAll(req, res));
+
+router.get('/:id', (req, res) => partyController.get(req, res));
+
+router.delete('/:id', (req, res) => partyController.delete(req, res));
+
+router.put('/:id', (req, res) => partyController.update(req, res));
+
 export default router;
